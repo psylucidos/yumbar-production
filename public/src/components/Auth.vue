c4e66cb4816a5225b352354783b65a67d56a8044
psylucidos <psylucidos@gmail.com>
2022-02-14 14:16:54 +1100

    Created frontend layout and added auth page

---
+++ public/src/components/Auth.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-card class="auth-card">
+    <div class="text-h6 text-center">
+      Login
+    </div>
+    <div class="">
+      <q-form
+        @submit="onSubmit"
+        class="q-pa-md"
+      >
+        <q-input
+          v-model="username"
+          label="Username"
+          :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+        <q-input
+          v-model="password"
+          type="password"
+          label="Password"
+          :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+        <q-btn label="Submit" type="submit" color="primary"/>
+      </q-form>
+    </div>
+  </q-card>
+</template>
+
+<style media="screen" lang="scss" scoped>
+  .auth-card {
+    padding: 2vh;
+    width: 30vw;
+  }
+
+  .q-btn {
+    width: 100%;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .auth-card {
+      width: 60vw;
+    }
+  }
+</style>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Auth',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.username, this.password); //eslint-disable-line
+    },
+  },
+});
+</script>
--- public/src/pages/timesheet.vue
+++ public/src/pages/timesheet.vue
@@ -1,17 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <AuthComponent />
+
   </q-page>
 </template>
 
 <script>
-import AuthComponent from 'components/Auth.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex',
-  components: {
-    AuthComponent,
-  },
+  name: 'PageTimesheet',
 });
 </script>
