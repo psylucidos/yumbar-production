fca8ddc7be38b2644d26e134c291d6e58ebebd45
psylucidos <psylucidos@gmail.com>
2022-02-22 13:25:35 +1100

    Created UI frame for stats page

---
+++ public/src/components/IcecreamFlavourChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <DoughnutChart :chartData="testData" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
+export default defineComponent({
+  name: 'IcecreamFlavourChart',
+  components: { DoughnutChart },
+  setup() {
+    const testData = {
+      labels: ['Vanilla', 'Chocolate', 'Hazelnut', 'Jaffa'],
+      datasets: [
+        {
+          data: [50, 30, 15, 5],
+          backgroundColor: ['yellow', 'brown', 'beige', 'orange'],
+        },
+      ],
+    };
+
+    return { testData };
+  },
+});
+</script>
---
+++ public/src/components/ProductionDataTable.vue
@@ -0,0 +1,66 @@
+<template lang="html">
+  <q-markup-table class="no-shadow">
+    <thead>
+      <tr>
+        <th class="text-left">Dessert (100g serving)</th>
+        <th class="text-right">Calories</th>
+        <th class="text-right">Fat (g)</th>
+        <th class="text-right">Carbs (g)</th>
+        <th class="text-right">Protein (g)</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="text-left">Frozen Yogurt</td>
+        <td class="text-right">159</td>
+        <td class="text-right">6</td>
+        <td class="text-right">24</td>
+        <td class="text-right">4</td>
+      </tr>
+      <tr>
+        <td class="text-left">Ice cream sandwich</td>
+        <td class="text-right">237</td>
+        <td class="text-right">9</td>
+        <td class="text-right">37</td>
+        <td class="text-right">4.3</td>
+      </tr>
+      <tr>
+        <td class="text-left">Eclair</td>
+        <td class="text-right">262</td>
+        <td class="text-right">16</td>
+        <td class="text-right">23</td>
+        <td class="text-right">6</td>
+      </tr>
+      <tr>
+        <td class="text-left">Cupcake</td>
+        <td class="text-right">305</td>
+        <td class="text-right">3.7</td>
+        <td class="text-right">67</td>
+        <td class="text-right">4.3</td>
+      </tr>
+      <tr>
+        <td class="text-left">Gingerbread</td>
+        <td class="text-right">356</td>
+        <td class="text-right">16</td>
+        <td class="text-right">49</td>
+        <td class="text-right">3.9</td>
+      </tr>
+    </tbody>
+  </q-markup-table>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProductionDataTable',
+  // data() {
+  //   return {
+  //
+  //   };
+  // },
+});
+</script>
