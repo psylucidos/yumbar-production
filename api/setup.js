e889ecc4a5e0fc9f9edcc94da76106cc5479a50e
psylucidos <psylucidos@gmail.com>
2022-03-09 16:29:11 +1100

    Updated db setup script to work when tables dont exist

--- api/setup.js
+++ api/setup.js
@@ -8,17 +8,18 @@ const clearQuery = fs.readFileSync('./cleardb.sql', { encoding: 'utf8', flag: 'r
 
 console.log('Clearing database tables...');
 db.query(clearQuery, [], (clearErr, clearRes) => {
+  console.log(clearRes);
+  console.log('Setting up database tables...');
+  db.query(setupQuery, [], (err, res) => {
+    if (err) {
+      console.log('Looks like table(s) already exist!');
+    } else {
+      console.log('Created tables!');
+      console.log(res.rows);
+    }
+  });
+
   if (clearErr) {
-    throw clearErr;
-  } else {
-    console.log(clearRes.rows);
-    console.log('Setting up database tables...');
-    db.query(setupQuery, [], (err, res) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log(res.rows);
-      }
-    });
+    console.log('Looks like tables dont exist, oh well, it tried.');
   }
 });
